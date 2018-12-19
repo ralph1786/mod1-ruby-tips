fe529f4d440ae59197435dd148a307f0b8a5ebc5
ralph1786 <ralph1786@gmail.com>
2018-12-19 14:00:47 -0500

    Done

--- solution.rb
+++ solution.rb
@@ -1,1 +1,14 @@
+def palindrome?(string)
+    if string.split("").reverse().join() == string 
+        puts true 
+    else 
+        puts false
+    end
+end
+
+palindrome?("racecar")
+palindrome?("noon")
+palindrome?("five")
+palindrome?("radar")
+palindrome?("learnlovecode")
 
